35e5338a7a6ecde000a6436b74a8a45684670a59
Eliurkis Diaz <me@eliurkis.com>
2018-01-12 13:47:40 -0500

    Add i-checks class to checkbox type to be used by a customer library

--- src/FieldTypes/Foreign.php
+++ src/FieldTypes/Foreign.php
@@ -9,7 +9,7 @@ class Foreign
         // Define the total of columns
         $colsSize = 12 / $properties['config']['cols'];
 
-        $html = '<div class="row">';
+        $html = '<div class="row i-checks">';
         foreach ($options as $key => $option) {
             $uniqueId = uniqid('opt', true).md5(mt_rand(1, 1000));
             $html .=
